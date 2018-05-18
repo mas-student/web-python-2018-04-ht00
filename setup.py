fee0c7a5f17485258fdff532f3fd3507663bc57e
mas-student <35464776+mas-student@users.noreply.github.com>
2018-05-18 07:57:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,12 @@ from os.path import join, dirname
 import os
 
 setup(
-    name='grammaranalyzer',
+    name='INPUT YOUR PROJECT NAME',
     version='1.0',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
-        "nltk",
     ],
-    test_suite='nose.collector',
     tests_require=[
-        'nose',
     ],
 )
